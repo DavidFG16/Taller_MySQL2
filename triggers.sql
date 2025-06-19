11425d2f6d95ba4743747951de7c4f15d618495d
DavidFG16 <189179517+DavidFG16@users.noreply.github.com>
2025-06-19 16:49:33 -0500

    feat: :sparkles: Primer trigger completado

--- triggers.sql
+++ triggers.sql
@@ -1,1 +1,23 @@
 USE pizzas;
+
+-- PUNTO 1
+
+DELIMITER // 
+DROP TRIGGER IF EXISTS tg_before_insert_detalle_pedido //
+
+CREATE TRIGGER IF NOT EXISTS tg_before_insert_detalle_pedido
+BEFORE INSERT ON detalle_pedido
+FOR EACH ROW
+BEGIN
+    IF NEW.cantidad < 1 THEN
+        SIGNAL SQLSTATE '40001'
+         SET MESSAGE_TEXT = 'La cantidad de ser minimo 1';
+
+    END IF;
+END //
+DELIMITER ;
+
+INSERT INTO detalle_pedido VALUES(NULL, LAST_INSERT_ID(), -1);
+
+
+-- PUNTO 2
